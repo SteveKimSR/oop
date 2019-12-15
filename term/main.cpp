549fe06590d84e357475a1134fad689ed2dffd0f
SteveKimSR <kimsr96@naver.com>
2019-12-16 04:42:57 +0900

    뿌요뿌요 텀프로젝트

---
+++ term/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "puyo.h"
+
+int main(){
+	puyo* a = new puyo();
+	a->run();
+	return 1;
+}
---
+++ term/puyo.h
@@ -0,0 +1,37 @@
+#ifndef PUYO_H
+#define	PUYO_H
+
+#include <iostream>
+
+class Gmap;											// Declaration of classes
+class pointer;
+class block;
+
+class puyo {
+
+private:
+	Gmap* gameMap;									
+	pointer* position;								
+	block* Block;
+
+	char insertKey;									// key from user
+	int score;										// score
+	bool onGoing;									// boolean checking if it needs to create a block or not
+public:
+	puyo();											// constructor
+
+	void set_seed(int);
+	int random(int);								// method for getting random int
+	void run();										// EXECUTING METHOD
+
+	void createBlock();								// method for making a new block
+	void printMap();								// method for printing map
+	void get_key();									// method for getting key
+	void key(char);									// method for key
+	void removeBlock(block*, pointer*);				// method deleting Block in Gmap
+	void pressX();
+	void explodeAndScore();							// method if collision has occured, explode and score
+
+};
+
+#endif
